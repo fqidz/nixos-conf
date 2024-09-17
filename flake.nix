8e42aa97c5450086bf88b976ec45bc85cb6c952f
fqidz <meowthful127@gmail.com>
2024-09-17 22:01:50 +0300

    added formatter

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
     system = "x86_64-linux";
   in
   {
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       inherit system;
       specialArgs = { inherit inputs; };
