f201f5513f671c48759352e6a15413404fc2fd30
fqidz <faidz.arante@gmail.com>
2025-02-10 22:55:39 +0300

    fixed cliphist service

--- config/packages/displayManager/cliphist.nix
+++ config/packages/displayManager/cliphist.nix
@@ -8,14 +8,20 @@
   systemd.user.services.cliphist-history = {
     Unit = {
       Description = "Stores clipboard history using cliphist";
+      After = "graphical-session.target";
+      PartOf = "graphical-session.target";
     };
     Install = {
       WantedBy = [ "graphical-session.target" ];
     };
     Service = {
+      ExecSearchPath = [
+        "${pkgs.cliphist}/bin"
+        "${pkgs.wl-clipboard}/bin"
+      ];
       ExecStart = "${pkgs.writeShellScript "cliphist-history" ''
         #!${pkgs.bash}/bin/sh
-        wl-paste --watch cliphist store
+        ${pkgs.wl-clipboard}/bin/wl-paste --watch cliphist store
       ''}";
     };
   };
