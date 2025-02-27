445f96f90eb4cca090866b641c3d358ad60f407e
fqidz <faidz.arante@gmail.com>
2025-02-27 10:06:46 +0300

    removed github ssh over https

--- config/home.nix
+++ config/home.nix
@@ -144,12 +144,6 @@ in
 
     ssh = {
       enable = true;
-      extraConfig = ''
-        Host github.com
-            Hostname ssh.github.com
-            Port 443
-            User git
-      '';
     };
   };
 
