0e558a6056ace0b44f6e190ebe4b37c29b6a551e
fqidz <faidz.arante@gmail.com>
2025-08-13 19:42:13 +0200

    feat: update db env secrets

--- modules/home-manager/podman-quadlet/containers/timescaledb.nix
+++ modules/home-manager/podman-quadlet/containers/timescaledb.nix
@@ -7,6 +7,25 @@
   # https://dl.thalheim.io/
   sops.secrets = {
     "db.env".sopsFile = ../../../../secrets/db.yaml;
+    "pguser".sopsFile = ../../../../secrets/db.yaml;
+    "pgpassword".sopsFile = ../../../../secrets/db.yaml;
+    "pghost".sopsFile = ../../../../secrets/db.yaml;
+    "pgport".sopsFile = ../../../../secrets/db.yaml;
+    "pgdatabase".sopsFile = ../../../../secrets/db.yaml;
+  };
+
+  sops.templates."pguser".content = "${config.sops.placeholder."pguser"}";
+  sops.templates."pgpassword".content = "${config.sops.placeholder."pgpassword"}";
+  sops.templates."pghost".content = "${config.sops.placeholder."pghost"}";
+  sops.templates."pgport".content = "${config.sops.placeholder."pgport"}";
+  sops.templates."pgdatabase".content = "${config.sops.placeholder."pgdatabase"}";
+
+  home.sessionVariables = {
+    PGUSER = "$(cat ${config.sops.templates."pguser".path})";
+    PGPASSWORD = "$(cat ${config.sops.templates."pgpassword".path})";
+    PGHOST = "$(cat ${config.sops.templates."pghost".path})";
+    PGPORT = "$(cat ${config.sops.templates."pgport".path})";
+    PGDATABASE = "$(cat ${config.sops.templates."pgdatabase".path})";
   };
 
   # https://seiarotg.github.io/quadlet-nix/home-manager-options.html
