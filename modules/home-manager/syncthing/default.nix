9b964a29a512c81d55806bcc19a67075355e06d3
fqidz <faidz.arante@gmail.com>
2025-09-14 10:15:22 +0300

    fix: syncthing systemd not starting due to invalid arg

--- modules/home-manager/syncthing/default.nix
+++ modules/home-manager/syncthing/default.nix
@@ -4,7 +4,6 @@
     syncthing = {
       enable = true;
       extraOptions = [
-        "--no-default-folder"
         "--no-browser"
         "--config=${config.xdg.configHome}/syncthing"
         "--data=${config.xdg.dataHome}/syncthing"
