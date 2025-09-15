f4b53042b453ba6fbf3e605028374bf1c45dcf16
fqidz <faidz.arante@gmail.com>
2025-09-15 23:08:11 +0300

    feat: various
    eww:
    - improve workspaces.sh
    - change 'id' to 'name' for workspaces
    - add spotify in workspaces
    - remove battery.sh
    
    other:
    - remove monitor-wake-conf

--- modules/home-manager/desktop-environment/eww/eww-config/scripts/workspaces.sh
+++ modules/home-manager/desktop-environment/eww/eww-config/scripts/workspaces.sh
@@ -1,24 +1,64 @@
 #!/usr/bin/env bash
 
-get_workspace() {
-    active_workspace=$(hyprctl monitors -j | jq -r '.[0].activeWorkspace.id')
-    workspaces_json=$(
+# takes the name of the active workspace as arg 1
+get_workspaces_json() {
+    active_workspace=$1
     hyprctl workspaces -j \
         | jq -rc \
-        --arg ACTIVE_ID $active_workspace \
-        'map( { id: .id | select( . > 0 ) | tostring } ) |
-        map( . + { "active": (if .id != $ACTIVE_ID then false else true end) } ) | sort_by ( .id )'
-    )
-    echo "${workspaces_json}"
+        --arg ACTIVE_NAME $active_workspace \
+        'map( { name, "active": (if .name != $ACTIVE_NAME then false else true end) } ) | sort_by ( .name )'
+}
+
+get_active_workspace() {
+    echo "$(hyprctl monitors -j | jq -r '.[0].activeWorkspace.id')"
 }
 
 process_event() {
-    if [[ ${1%>>*} == "createworkspace" || ${1%>>*} == "destroyworkspace" || ${1%>>*} == "workspace" ]]; then
-        get_workspace
+    # Get the first parameter of the string, delimited by '>>' (i.e. the text
+    # before '>>')
+    # eg. 'workspace>>4' we will get 'workspace'
+    event=${1%>>*}
+
+    # Get the last parameter of the string, delimited by '>>' (i.e. the text
+    # after '>>')
+    # eg. 'createworkspace>>2' we will get '2'
+    event_info=${1##*>>}
+
+    # More info on parameter expansion here: https://mywiki.wooledge.org/BashFAQ/073
+
+    if [[ $event == "destroyworkspace" ]]; then
+        # The event_info outputted here is the name of the workspace that was
+        # destroyed, so we have to get the active workspace name instead.
+        get_workspaces_json $(get_active_workspace)
+
+    elif [[ $event == "createworkspace" || $event == "workspace" ]]; then
+        # The event_info text only contains the name of the workspace for these events.
+        get_workspaces_json "$event_info"
+
+    elif [[ $event == "activespecialv2" ]]; then
+        # This is for getting the active workspace for special workspaces.
+        #
+        # The event_info text here would be something like this:
+        #   `-98,special:spotify,eDP-1`
+        # which is:
+        #   `workspace_id,workspace_name,monitor`
+        # We only need the workspace_name so we get only that.
+        #
+        # When leaving the special workspace, the text outputted would be:
+        #   `,,eDP-1`
+        # We get an empty string for the workspace_name, so we just get the
+        # active workspace name instead.
+        workspace_name=$(cut -d ',' -f2 <<< $event_info)
+        if [[ -n $workspace_name ]]; then
+            get_workspaces_json $workspace_name
+        else
+            get_workspaces_json $(get_active_workspace)
+        fi
     fi
 }
 
-get_workspace
+# Output once when the script starts
+get_workspaces_json $(get_active_workspace)
 
 socat -u "UNIX-CONNECT:${XDG_RUNTIME_DIR}/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock" - | while read -r event; do
     process_event "${event}"
