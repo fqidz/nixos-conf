046ebfc0c8a382168f6f6841ae9bed182aab921b
fqidz <faidz.arante@gmail.com>
2026-07-13 18:04:14 +0300

    fix nginx conf

--- modules/nixos/nginx/default.nix
+++ modules/nixos/nginx/default.nix
@@ -88,11 +88,13 @@
         locations."/" = {
           proxyPass = "http://127.0.0.1:8080";
           extraConfig = ''
-            proxy_set_header Host $host;
+            #proxy_set_header Host $host;
 
-            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-Host $host:$server_port;
+            proxy_set_header X-Forwarded-Server $host;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Proto https;
+            proxy_set_header X-Real-IP $remote_addr;
           '';
         };
       };
