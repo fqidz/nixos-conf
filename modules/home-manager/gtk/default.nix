2c86257041b0d9a3614b0c8898cb91e4b390f44b
fqidz <faidz.arante@gmail.com>
2026-09-11 02:16:15 +0300

    fix dark mode firefox

--- modules/home-manager/gtk/default.nix
+++ modules/home-manager/gtk/default.nix
@@ -2,12 +2,7 @@
 {
   gtk = {
     enable = true;
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
-    };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
-    };
+    colorScheme = "dark";
     cursorTheme = {
       package = pkgs.rose-pine-cursor;
       name = "BreezeX-RosePine-Linux";
